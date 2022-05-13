2be3aec6956b2fde6a12d07eb64e502c4f1e6bf2
unknown <oliviersndeye>
2022-05-13 13:05:02 -0300

    Update do arquivo App. aula de context API

--- src/App.js
+++ src/App.js
@@ -1,54 +1,12 @@
 import React, { Component } from "react";
-import Twitter from "./Twitter";
 
 
-class App extends Component {
+function App() {
+  return(
+    <div style={{color: "lightblue", fontSize: "40px"}}>
+      App react 
+    </div>
+  )
+}
 
-  state = {
-    loading: false,
-    actived: true
-  }
-
-  componentDidMount() {
-    setTimeout(()=>{
-      this.setState({
-        loading: true
-      })
-    }, 3000) 
-  }
-
-  onRemove = () => {
-    this.setState({
-      actived: false
-    })
-  }
-
-
-  render() {
-
-    const posts = [{
-      title: 'xpto',
-      discription: 'foo'
-    },{
-      title: 'new york times',
-      discription: 'bar'
-    },{
-      title: 'treze pessoas enganadas',
-      discription: 'chaves'
-    },{
-      title: 'freza, pq vc matou o krilin',
-      discription: 'dbz',
-      date: 19990305
-    }]
-
-    return (
-      <div>
-      <button onClick={this.onRemove}> 
-        Remover componentes 
-      </button>
-      {this.state.actived && (<Twitter posts={posts} loading={this.state.loading} />)} 
-      </div>
-    )
-   }
-  }
 export default App
