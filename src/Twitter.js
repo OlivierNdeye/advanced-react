30ffff71069f14e4ba69a975b520b32b36f74da6
unknown <oliviersndeye>
2022-05-13 00:29:59 -0300

    update do arquivo Twitter do projeto

--- src/Twitter.js
+++ src/Twitter.js
@@ -1,33 +1,62 @@
-import React, { Component } from "react"
+import React, { useEffect, useState, memo} from "react"
 
-class Twitter extends Component {
-    
-   componentDidMount(){
-     const {posts, loading} = this.props
-     console.log('componentDidMount', posts)
-     console.log('componentDidMount:loading', loading)
-   }
-
-   componentDidUpdate(prevProps){
-     const { loading } = this.props
-      if(this.props.loading !== prevProps.loading) {
-        console.log('componentDidUpdate:loading', loading)
-      }
-   }
-
-   componentWillUnmount(){
-     console.log('componentWillUnmount: fui removido ;) ')
-   }
-
-  render() {
-    const {posts} = this.props
-    console.log('render', posts)
-    return (
-      <div>
-        react tests
-      </div>
-    )
-  }
+const areEqual = (prevProps, nextProps) => {
+  return prevProps.loading === nextProps.loading
 }
 
-export default Twitter
+function Twitter(props) {
+ const {loading} = props
+ const [tweet, setTweet] = useState('title')
+
+
+   //uso do useEffect é similar ao componentDidMount()
+   useEffect(() => {
+    const {posts, loading} = props
+    console.log('componentDidMount', posts)
+    console.log('componentDidMount:loading', loading)
+   }, [])
+
+   //ComponentDidUpdate()
+   useEffect(() =>{
+     console.log('componentDidUpdate', loading)
+   }, [loading])
+
+   //componentWillUnmout
+   useEffect(() => {
+     return() => {
+       console.log('componentWillUnmout: Removido com sucesso!')
+     }
+   }, [])
+
+  //  componentDidUpdate = (prevProps) => {
+  //    const { loading } = props
+  //     if(props.loading !== prevProps.loading) {
+  //       console.log('componentDidUpdate:loading', loading)
+  //     }
+  //  }
+
+  //  componentWillUnmount =   () => {
+  //    console.log('componentWillUnmount: fui removido ;) ')
+  //  }
+
+  //  shouldComponentUpdate = (nextProps, nextState) => {
+  //    return state.tweet !== nextState.tweet || nextProps.loading !== this.props.loading
+  //  }
+
+   const handleTweet = () => {
+     setTweet('Tweet atualizado com sucesso')
+   }
+
+  console.log('Tweet Atualizado', tweet)
+
+  return (
+     <div>
+       <button onClick={handleTweet}>
+         re-tweet
+       </button>
+       <p style={{color: "lightblue"}}> Esta é a Aula reactjs </p>
+     </div>
+   )
+}
+
+export default memo(Twitter, areEqual)
