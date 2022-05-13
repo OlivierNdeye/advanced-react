0e86d205bcf46a227ea12c79542fde53bcc78822
unknown <oliviersndeye>
2022-05-13 14:26:26 -0300

    update do arquivo App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
+import { ThemeContext, themes } from './Theme'
+import Card from './Card'
 
-
-function App() {
+function App () {
   return(
-    <div style={{color: "lightblue", fontSize: "40px"}}>
-      App react 
+    <div style={{color:"lightblue", fontSize:"40px"}}>
+      Precione aqui !
+      <ThemeContext.Provider value={themes.primary}>
+      <Card />
+      </ThemeContext.Provider>
     </div>
+      
   )
 }
 
